b5384a33d836786b6aad7ce9b1c824140d149f4a
Faizi Sheikh <faizisheikh700@gmail.com>
2023-09-01 16:24:41 +0500

    assign

---
+++ assign/app.js
@@ -0,0 +1,73 @@
+
+// chapter 1, Q:NO: 1,2
+// alert('Enter! aa valid password')
+
+// chapter 1, Q:NO: 3
+// alert('Welcome to JS land...\nHappy coding!')
+
+// chapter 1, Q:NO: 4
+// alert('Welcome to JS land') 
+// var b = 'Happy Codin \nPrevent this page creating additional dialogs' 
+// alert(b)
+
+// chapter 1, Q:NO: 5
+// alert("Hello... I can run JS through my web browser's console")
+
+// chapter 2,  
+// 1. Declare a variable called username.
+// var username;
+
+// // 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+// var myName = "Your Full Name";
+
+// // 3. Write a script to
+// // a. declare a JS variable, titled message
+// var message;
+// // b. assign “Hello World” to the variable message
+// message = "Hello World";
+// // c. display the message in an alert box
+// alert(message);
+
+// // 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+// var studentName = "SHEIKH MUHAMMAD FAIZAN";
+// var age = 24;
+// var course = "Web and Mobile app";
+
+// alert("Student Name: " + studentName);
+// alert("Age: " + age);
+// alert("Course: " + course);
+
+// Q: 5
+// alert('PIZZA\nPIZZ\nPIZ\nPI\nP')
+
+// Q; 6
+// alert('You wil be a software developer in USA, and married to Aliya with 2 kids')
+
+//Q: 7
+// var email =  faizisheikh700@gmail.com
+
+// alert('My email address is' + email)
+
+// Q: 8
+
+// var book = 'A smarter way to learn javaScript'
+
+// alert(book)
+
+// Q: 9
+// var studentName = "SHEIKH MUHAMMAD FAIZAN";
+// var age = 24;
+// var course = "Development";
+// var module = "Web and Mobile app";
+
+// // Display the student's bio data with meaningful messages
+// alert("My name is " + studentName);
+// alert("I am " + age + " years old");
+// alert("I study in SSUET");
+// alert("I take " + course + " classes");
+// alert("I am enrolled in the " + module + " course");
+
+// Q: 10
+// var msg = 'Yay! I wrote HTML content through JavaScript'
+
+// document.write(msg)
